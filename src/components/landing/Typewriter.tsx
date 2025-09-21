d0d04d2a1ed45406bfef0a0222595c863cb60748
Steffin Thomas <steffinthomas12@gmail.com>
2025-09-21 11:35:31 +0000

    make animation of typewriting ion loop here (_for element div_)

--- src/components/landing/Typewriter.tsx
+++ src/components/landing/Typewriter.tsx
@@ -5,25 +5,41 @@ import { useState, useEffect } from 'react';
 type TypewriterProps = {
   text: string;
   speed?: number;
+  loopDelay?: number;
 };
 
-export function Typewriter({ text, speed = 50 }: TypewriterProps) {
+export function Typewriter({ text, speed = 50, loopDelay = 1500 }: TypewriterProps) {
   const [displayedText, setDisplayedText] = useState('');
 
   useEffect(() => {
     let i = 0;
-    setDisplayedText(''); // Reset on text change
-    const timer = setInterval(() => {
+    let isMounted = true;
+    let timeoutId: NodeJS.Timeout;
+
+    const type = () => {
+      if (!isMounted) return;
+
       if (i < text.length) {
         setDisplayedText(prev => prev + text.charAt(i));
         i++;
+        timeoutId = setTimeout(type, speed);
       } else {
-        clearInterval(timer);
+        // Pause at the end, then reset
+        timeoutId = setTimeout(() => {
+          i = 0;
+          setDisplayedText('');
+          type();
+        }, loopDelay);
       }
-    }, speed);
+    };
 
-    return () => clearInterval(timer);
-  }, [text, speed]);
+    type();
+
+    return () => {
+      isMounted = false;
+      clearTimeout(timeoutId);
+    };
+  }, [text, speed, loopDelay]);
 
   return (
     <span>
