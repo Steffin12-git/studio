237ad8e1ff9f12a18b953eb4704ec65b19c3d80f
Steffin Thomas <steffinthomas12@gmail.com>
2025-09-21 17:31:42 +0000

    add some background images and add translucent vlured effect also

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import { Poppins, Lexend } from 'next/font/google'
+import { Poppins, Lexend } from 'next/font/google';
+import { BackgroundBlobs } from '@/components/common/BackgroundBlobs';
 
-const poppins = Poppins({ 
+const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '600', '700', '900'],
   variable: '--font-poppins',
 });
-const lexend = Lexend({ 
+const lexend = Lexend({
   subsets: ['latin'],
   variable: '--font-lexend',
 });
 
-
 export const metadata: Metadata = {
   title: 'My Awesome Portfolio',
-  description: "A portfolio of awesome projects.",
+  description: 'A portfolio of awesome projects.',
 };
 
 export default function RootLayout({
@@ -26,8 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} ${lexend.variable} font-body antialiased`}>
-        {children}
+      <body
+        className={`${poppins.variable} ${lexend.variable} font-body antialiased relative`}
+      >
+        <BackgroundBlobs />
+        <div className="relative z-10">{children}</div>
         <Toaster />
       </body>
     </html>
