998119000b6b90dd1f53cba7e61ec1efc8098b4c
Steffin Thomas <steffinthomas12@gmail.com>
2025-09-25 17:10:03 +0000

    in this carousel after numpy this list sudenly stops and starts from fir

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -103,10 +103,19 @@ const config = {
             height: '0',
           },
         },
+        'scroll-x': {
+          from: {
+            transform: 'translateX(0)',
+          },
+          to: {
+            transform: 'translateX(-50%)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'scroll-x': 'scroll-x 30s linear infinite',
       },
     },
   },
