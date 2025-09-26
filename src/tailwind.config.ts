18977716da0234432fbf84abb1501c564ed43e0c
Steffin Thomas <steffinthomas12@gmail.com>
2025-09-26 12:08:38 +0000

    increase the speed of the carousel\ (_for element div_)

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -116,7 +116,7 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'scroll-x': 'scroll-x 40s linear infinite',
+        'scroll-x': 'scroll-x 25s linear infinite',
       },
     },
   },
