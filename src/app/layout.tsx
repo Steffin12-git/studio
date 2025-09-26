0e0d60766c8af8e8e05713a9326e1ec189727266
Steffin Thomas <steffinthomas12@gmail.com>
2025-09-26 14:47:04 +0000

    change the bacground to ultra clear mooving stars or cosmosis, it should

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import './cosmos.css';
 import { Toaster } from '@/components/ui/toaster';
 import '@fontsource/playfair-display'; // Supports weights 400-900
 import '@fontsource/pt-sans'; // Supports weights 400, 700
@@ -22,7 +23,9 @@ export default function RootLayout({
         className={`font-body antialiased relative`}
       >
         <div className="fixed inset-0 -z-20 h-full w-full bg-background">
-          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+          <div className="stars"></div>
+          <div className="stars2"></div>
+          <div className="stars3"></div>
         </div>
         <div className="relative z-10">{children}</div>
         <Chatbot />
