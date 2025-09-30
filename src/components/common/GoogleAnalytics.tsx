835199b2d89ff6d18adf381d94ed0c810e31c1e8
Steffin Thomas <steffinthomas12@gmail.com>
2025-09-30 11:02:17 +0000

    Your Google tag wasn't detected on "www.steffinthomas.com". Try again or

--- src/components/common/GoogleAnalytics.tsx
+++ src/components/common/GoogleAnalytics.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Script from 'next/script';
@@ -6,32 +5,34 @@ import Script from 'next/script';
 const GoogleAnalytics = () => {
   const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
-  // Don't render scripts if the measurement ID is not set
-  if (!measurementId) {
-    console.warn("Google Analytics Measurement ID is not set. Analytics will be disabled.");
-    return null;
-  }
-
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${measurementId}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      {measurementId ? (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${measurementId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
+      ) : (
+        <script>
+          {console.warn("Google Analytics Measurement ID is not set. Analytics will be disabled.")}
+        </script>
+      )}
     </>
   );
 };
