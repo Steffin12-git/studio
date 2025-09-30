36da7ceba2b57c4ef107a8490742c30cc2155b8c
Steffin Thomas <steffinthomas12@gmail.com>
2025-09-30 11:03:53 +0000

    add the scriopt as it was given so that there will not be any porobelm,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import '@fontsource/playfair-display'; // Supports weights 400-900
 import '@fontsource/pt-sans'; // Supports weights 400, 700
 import Image from 'next/image';
 import Chatbot from '@/components/common/Chatbot';
-import GoogleAnalytics from '@/components/common/GoogleAnalytics';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Steffin Thomas | Data Analyst Portfolio',
@@ -20,10 +20,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <head>
+        {/* Google tag (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-8WGT855Z09" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8WGT855Z09');
+          `}
+        </Script>
+       </head>
        <body
         className={`font-body antialiased relative`}
       >
-        <GoogleAnalytics />
         <div className="fixed inset-0 -z-20 h-full w-full bg-background">
         </div>
 
