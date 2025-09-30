c8c75676e7f5c990b544024ce9a7010e0c57304f
Steffin Thomas <steffinthomas12@gmail.com>
2025-09-30 11:10:36 +0000

    Ownership verification failed Verification method: Google Analytics Fail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,13 +22,15 @@ export default function RootLayout({
     <html lang="en">
        <head>
         {/* Google tag (gtag.js) */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-8WGT855Z09" />
-        <Script id="google-analytics">
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-8WGT855Z09"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-8WGT855Z09');
           `}
         </Script>
